8ca9562f1543110b5f1e815e04fd22647eea3fe5
Charles <46895814+CharlesAnz@users.noreply.github.com>
2021-03-15 14:31:51 -0500

    added models to scene and started work on animations

--- Arthur Narrative Game/Assets/Scripts/Interactable.cs
+++ Arthur Narrative Game/Assets/Scripts/Interactable.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-    public float radius = 3f;
+    public float radius;
     public Transform interactionTransform;
 
     protected bool isFocus = false;
