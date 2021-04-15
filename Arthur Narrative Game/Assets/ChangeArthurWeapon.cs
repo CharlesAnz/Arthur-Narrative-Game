6c7f63a425cb0044011179a083a5b9373bbb3d16
Charles <46895814+CharlesAnz@users.noreply.github.com>
2021-04-15 15:54:52 -0500

    added box collider that changes arthur's weapons and moves him into boss arena

---
+++ Arthur Narrative Game/Assets/ChangeArthurWeapon.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChangeArthurWeapon : MonoBehaviour
+{
+    public GameObject sword;
+
+    public GameObject torch;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            sword.SetActive(true);
+            torch.SetActive(false);
+
+            other.transform.position = transform.position + (transform.forward * -6);
+
+            other.GetComponent<NavMeshAgent>().SetDestination(transform.position + (transform.forward * -7));
+
+            gameObject.GetComponent<BoxCollider>().isTrigger = false;
+        }
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(
+             transform.position + (transform.forward * -7),
+            new Vector3(4, 4, 4));
+
+    }
+}
